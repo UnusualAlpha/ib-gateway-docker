9082c059d9dd1a267bbb1afa327cd8bbc505687c
Emanuel Fernandes <emanuel.fernandes@flutter.com>
2022-07-24 18:58:03 +0100

    add new ibgateway stable and latest releases

---
+++ ibgateway-releases/ibgateway-10.12.2p-standalone-linux-x64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:538e74ed9a8111cd6bfc8839e83f44da64d1906d67323252b35aedb6a704cfb9
+size 176374542
---
+++ ibgateway-releases/ibgateway-10.16.1n-standalone-linux-x64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:af2c8e51271e139bf3a2d30ce849c6d34a5a873c0a1d3f4ad465293d3c032a30
+size 177310298
